54fb79eda4c6d42d8106fbb68be6222c1d986021
Rehaan99 <rehaanmp@hotmail.co.uk>
2022-05-03 00:36:23 +0100

    Added additional levels

--- levelData.js
+++ levelData.js
@@ -8,3 +8,23 @@ class LevelInfo {
 }
 //level 1
 levelData.push(new LevelInfo(300, 30, 600));
+//level 2
+levelData.push(new LevelInfo(300, 50, 600));
+//level 3
+levelData.push(new LevelInfo(300, 50, 500));
+//level 4
+levelData.push(new LevelInfo(200, 50, 500));
+//level 5
+levelData.push(new LevelInfo(200, 30, 600));
+//level 6
+levelData.push(new LevelInfo(200, 30, 600));
+//level 7
+levelData.push(new LevelInfo(300, 30, 600));
+//level 8
+levelData.push(new LevelInfo(300, 30, 600));
+//level 9
+levelData.push(new LevelInfo(300, 30, 600));
+//level 10
+levelData.push(new LevelInfo(500, 150, 200));
+//level 11
+levelData.push(new LevelInfo(300, 30, 600));
